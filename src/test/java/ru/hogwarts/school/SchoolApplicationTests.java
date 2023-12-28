c8ae559b12ced574dae65debc0d985735d793a66
nkbpp <alexbratchin@gmail.com>
2023-12-28 09:05:54 +0300

    Initial commit

---
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+
+}
