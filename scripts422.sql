928885fc64398fcb0963a0074a47cd0a1cdeb953
aleks <alexbratchin@gmail.com>
2024-02-02 18:16:38 +0300

    hw4.2 fix id

--- scripts422.sql
+++ scripts422.sql
@@ -1,12 +1,12 @@
 CREATE TABLE car (
-	Id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	brand varchar(255) CHECK(name !='') NOT NULL,
 	model varchar(255) CHECK(name !='') NOT NULL,
 	price float8 NOT NULL,
 	unique(brand, model)
 );
 CREATE TABLE person (
-	Id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name varchar(255) CHECK(name !='') NOT NULL,
 	age int8 DEFAULT 18 CHECK(Age >= 18 AND Age < 100) NOT NULL,
 	is_driver_license boolean DEFAULT false NOT NULL,
